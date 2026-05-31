65bcf5d43b98a09d7da27ede0c220ea96f1c52e7
yosha7 <153965990+yosha7@users.noreply.github.com>
2026-05-31 21:59:41 +0530

    Time: 0 ms (100%), Space: 13.1 MB (52.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+       seen={}
+       for i,num in enumerate(nums):
+        needed=target-num
+        if needed in seen:
+            return [seen[needed],i]
+        seen[num]=i
+        
+        
